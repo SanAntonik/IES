4688ec58b224fa9b65a890fe50354cd8011680ce
SanAntonik <sanantonik@gmail.com>
2024-03-16 11:08:53 +0200

    Rework POST method

--- hub/main.py
+++ hub/main.py
@@ -36,18 +36,33 @@ store_adapter = StoreApiAdapter(api_base_url=STORE_API_BASE_URL)
 app = FastAPI()
 
 
-@app.post("/processed_agent_data/")
+@app.post("/processed_agent_data")
 async def save_processed_agent_data(processed_agent_data: ProcessedAgentData):
-    redis_client.lpush("processed_agent_data", processed_agent_data.model_dump_json())
-    if redis_client.llen("processed_agent_data") >= BATCH_SIZE:
+    print()
+    logging.info("Inside save_processed_agent_data")
+    json_procesesd_agent_data = processed_agent_data.json()
+    logging.info(f"Got processed agent data: {json_procesesd_agent_data}")
+    redis_client.lpush("processed_agent_data", json_procesesd_agent_data)
+    logging.info("Pushed data to Redis queue")
+    queue_len = redis_client.llen("processed_agent_data")
+    logging.info(f"Current queue length: {queue_len}")
+    logging.info(f"BATCH_SIZE: {BATCH_SIZE}")
+    if queue_len >= BATCH_SIZE:
+        logging.info("Inside IF")
         processed_agent_data_batch: List[ProcessedAgentData] = []
         for _ in range(BATCH_SIZE):
-            processed_agent_data = ProcessedAgentData.model_validate_json(
-                redis_client.lpop("processed_agent_data")
-            )
+            logging.info("Inside FOR LOOP")
+            json_str = redis_client.lpop("processed_agent_data").decode("utf-8")
+            processed_agent_data = ProcessedAgentData.parse_raw(json_str)
             processed_agent_data_batch.append(processed_agent_data)
-        print(processed_agent_data_batch)
-        store_adapter.save_data(processed_agent_data_batch=processed_agent_data_batch)
+        logging.info("Exited FOR LOOP")
+        json_procesesd_agent_data_batch = [data.json() for data in processed_agent_data_batch]
+        logging.info("Processing batch of agent data:")
+        logging.info(json_procesesd_agent_data_batch)
+        store_adapter.save_data(processed_agent_data_batch=json_procesesd_agent_data_batch)
+        logging.info("Saved batch to store")
+    logging.info("Reached return")
+    print()
     return {"status": "ok"}
 
 
